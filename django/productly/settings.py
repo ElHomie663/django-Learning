14258b12f0226022aacdebaf5a07b8dff72d8f54
fredy arana <aranafredy80@gmail.com>
2026-07-22 23:45:06 -0400

    Configuro ALLOWED_HOSTS con asterisco para permitir conexiones

--- django/productly/settings.py
+++ django/productly/settings.py
@@ -32,9 +32,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vaaqaw&v=%&z%9%%qi(2m@n=pzu!_2tzsg_bu+gq*0zx1@486#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
